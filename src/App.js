09338850c0d8397cdd0cbdf77deff8c65b9f61a7
San-Di <oxfordvirus@gmail.com>
2018-06-14 23:01:06 +0630

    Latest push

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import Login from './containers/login';
 import Posts from './containers/Posts';
 import {BrowserRouter as Router , Route, Switch} from 'react-router-dom';
+import rootReducer from "./reducers/index";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
 
+const store = createStore(
+    rootReducer
+  );
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Provider store={store}>
       <Router>
         <Switch>
           <Route exact path = '/' render = {() => (<Login />)} />
           <Route path = '/posts' render = {() => (<Posts />)} />
         </Switch>
         </Router>
+        </Provider>
       </div>
+
     );
   }
 }
